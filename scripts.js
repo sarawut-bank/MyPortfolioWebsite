be3e09ed45798df7aed43ab0a0df5a41cfc7085d
sarawut-bank <sarwut123zz@gmail.com>
2023-08-11 04:07:52 +0700

    upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+function work1(){
+    console.log("work1")
+    var modal1 = document.getElementById("modal1");
+    modal1.style.display = "block";
+}
+
+function work2(){
+    console.log("work2")
+    var modal1 = document.getElementById("modal2");
+    modal1.style.display = "block";
+}
+
+function work3(){
+    console.log("work3")
+    var modal1 = document.getElementById("modal3");
+    modal1.style.display = "block";
+}
+
+function work4(){
+    console.log("work4")
+    var modal1 = document.getElementById("modal4");
+    modal1.style.display = "block";
+}
+
+function closeModal() {
+    console.log("close")
+    var modal = document.getElementById("modal1");
+    var modal2 = document.getElementById("modal2");
+    var modal3 = document.getElementById("modal3");
+    var modal4 = document.getElementById("modal4");
+    modal.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
+    modal4.style.display = "none";
+}
+
+
