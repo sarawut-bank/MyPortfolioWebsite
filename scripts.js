a6b007cac1ab8d7755f9cbea1b0e29265288c277
sarawut-bank <sarwut123zz@gmail.com>
2023-09-06 16:55:24 +0700

    update

--- scripts.js
+++ scripts.js
@@ -22,16 +22,24 @@ function work4(){
     modal1.style.display = "block";
 }
 
+function work5(){
+    console.log("work5")
+    var modal1 = document.getElementById("modal5");
+    modal1.style.display = "block";
+}
+
 function closeModal() {
     console.log("close")
     var modal = document.getElementById("modal1");
     var modal2 = document.getElementById("modal2");
     var modal3 = document.getElementById("modal3");
     var modal4 = document.getElementById("modal4");
+    var modal5 = document.getElementById("modal5");
     modal.style.display = "none";
     modal2.style.display = "none";
     modal3.style.display = "none";
     modal4.style.display = "none";
+    modal5.style.display = "none";
 }
 
 
